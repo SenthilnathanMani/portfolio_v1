6e4a4cb697c0a3508198f84863b722d7850f4494
Senthilnathan Mani <31062212+SENTHILnew@users.noreply.github.com>
2026-01-24 00:16:57 +0530

    fix header

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -6,7 +6,7 @@ import SocialLinks from "./social-links-icons/social-links";
 export default function Navbar() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 transition-all duration-300 bg-transparent">
-      <div className="mx-auto py-8  w-full">
+      <div className="mx-auto py-4  w-full">
         <nav className="flex items-center justify-evenly">
           <div className="text-white font-bold text-xl ">
             <div className="hover:scale-105 cursor-pointer transition-transform duration-200 text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent">
